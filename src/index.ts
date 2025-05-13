1fd5a24f6bbc7bfbd8ec1cc070577a8a0f695f30
Louise Aldridge <louisealdridge88@gmail.com>
2025-05-13 11:00:33 +0100

    payment functionality created

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@ type Order = {
     price: number
 }
 
-let orderTotal = 0
 let nextOrderId = 1
 let nextMenuItemId = 1
 
@@ -49,6 +48,12 @@ const cart: Order[] = []
 
 const menuRoot = document.getElementById("menu-root")!
 const cartRoot = document.getElementById("cart-root")!
+const cartTotalPrice = document.getElementById("cart-total-price")!
+const completeOrderBtn = document.getElementById("complete-order-btn")!
+const payModal = document.getElementById("pay-modal")!
+const closePayModalBtn = document.getElementById("close-pay-modal")!
+const payBtn = document.getElementById("pay-btn")!
+const paymentDetailsForm = document.getElementById("payment-details-form") as HTMLFormElement
 
 function renderMenu() {
 
@@ -65,7 +70,6 @@ function renderMenu() {
         </div>
         `
     }).join("")
-
 
     menuRoot.innerHTML = menuInnerHtml
 }
@@ -101,6 +105,10 @@ menuRoot.addEventListener("click", function (e) {
     }
 })
 
+function getCartTotal() {
+    return Object.values(cart).reduce((acc, item) => acc + item.price, 0)
+}
+
 function renderCart() {
     const cartInnerHtml = cart.map((cartItem: Order) => {
         return `
@@ -111,8 +119,31 @@ function renderCart() {
         `
     }).join("")
     cartRoot.innerHTML = cartInnerHtml
+    cartTotalPrice.textContent = "$" + getCartTotal()
 }
 
+completeOrderBtn.addEventListener("click", () => {
+    if (getCartTotal() > 0) {
+        payModal.style.display = "block"
+    }
+})
+
+closePayModalBtn.addEventListener("click", () => {
+    payModal.style.display = "none"
+})
+
+paymentDetailsForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const paymentFormData = new FormData(paymentDetailsForm)
+    const orderName = paymentFormData.get('name')
+    payModal.style.display = 'none'
+    document.getElementById("cart-section")!.style.display = "none"
+
+    document.getElementById("order-confirmation")!.style.display = "block"
+    document.getElementById("order-confirmation")!.innerHTML = `
+        <p> Thanks, ${orderName} Your order is on its way!</p>
+    `
+})
 
 renderMenu()
 
